b5d0b59246c95487812941b94ccc93b6dc03181c
wordsofwaking <100310998+wordsofwaking@users.noreply.github.com>
2022-04-10 10:26:14 -0700

    19.2FlaskJinjaAssignment

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+from markupsafe import escape
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
 
-@app.route('/welcome')
-def welcome():
-    return "welcome"
+debug = DebugToolbarExtension(app)
 
-@app.route('/welcome/back')
-def welcome_back():
-    return "welcome back"
+@app.route("/")
+def ask_questions():
+    """Create prompts based on response to correlate with Stories Object"""
 
-@app.route('/welcome/home')
-def welcome_home():
-    return "welcome home"
+    prompts = story.prompts
+
+    return render_template("base.html", prompts=prompts)
+
+
+@app.route("/madlib")
+def show_story():
+    """Display the story"""
+
+    text = story.generate(request.args)
+
+    return render_template("madlib.html", text=text)
+
+
+
