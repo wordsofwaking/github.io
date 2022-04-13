b1e517eff04c15cf940a8ce7f3f2bf47a40afd83
wordsofwaking <100310998+wordsofwaking@users.noreply.github.com>
2022-04-13 15:29:05 -0700

    19.3and4FlaskSurveyAssignment

--- app.py
+++ app.py
@@ -1,29 +1,71 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from stories import story
 from markupsafe import escape
+
+RESPONSES = "response"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
-
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-@app.route("/")
-def ask_questions():
-    """Create prompts based on response to correlate with Stories Object"""
-
-    prompts = story.prompts
-
-    return render_template("base.html", prompts=prompts)
 
 
-@app.route("/madlib")
-def show_story():
-    """Display the story"""
+@app.route('/')
+def start_survey():
+    """Explains and starts the survey"""
+    
+    session[RESPONSES] = []
 
-    text = story.generate(request.args)
-
-    return render_template("madlib.html", text=text)
+    return render_template("base.html")
 
 
+@app.route('/responses', methods=["POST"])
+def add_answer():
+    """Record survey answers"""
+    cust_answer = request.form['answer']
+    
+    response = session[RESPONSES]
+    response.append(cust_answer)
+    session[RESPONSES] = response
 
+    if (len(response) == 3):
+        return redirect('/thank_you')
+    
+    else:
+        return redirect(f"/question_{len(response)}/")
+
+
+@app.route('/question/0')
+def first_question():
+    """Ask the first question of the survey"""
+
+    return render_template("question_0.html")
+
+
+@app.route('/question/1')
+def second_question():
+    """Ask the second question of the survey"""
+
+    return render_template("question_1.html")
+
+
+@app.route('/question/2')
+def third_question():
+    """Ask the third question of the survey"""
+
+    return render_template("question_2.html")
+
+
+@app.route('/question/3')
+def last_question():
+    """Ask the last question of the survey"""
+
+    return render_template("question_3.html")
+
+
+@app.route('/thank_you')
+def redirect_to_home():
+    """Redirects to home page with flash message"""
+    flash('Thank you for taking our survey!')
+    return redirect("/")
